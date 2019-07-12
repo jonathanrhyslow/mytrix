450ce4ffd2b741e44b7a15e469e9583c32cbd830
THargreaves <tim.hargreaves@icloud.com>
2019-07-12 14:32:06 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='mytrix',
+      version='0.1',
+      description='A Python package for handling matrices and other \
+      linear algebra concepts',
+      url='https://github.com/THargreaves/mytrix',
+      author='Tim Hargreaves',
+      author_email='tim.hargreaves@icloud.com',
+      license='MIT',
+      packages=['mytrix'],
+      zip_safe=False)
