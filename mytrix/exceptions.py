a2c2e1120c257d559d353c23c471064ce45b6fec
THargreaves <tim.hargreaves@icloud.com>
2019-09-26 21:26:26 +0100

    Add linear algebra error

--- mytrix/exceptions.py
+++ mytrix/exceptions.py
@@ -5,3 +5,9 @@ class ComformabilityError(Exception):
     """An exception class for conformability errors."""
 
     pass
+
+
+class LinearAlgebraError(Exception):
+    """An exception class for linear algebra errors."""
+
+    pass
